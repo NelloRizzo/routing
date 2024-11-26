a332667cca66bcdd4a7db62fbb3502e55ecb7cbb
Nello Rizzo <nellorizzo@live.it>
2024-11-26 11:39:57 +0100

    Primo esempio

---
+++ reactrouting/src/About/About.js
@@ -0,0 +1,7 @@
+export function About() {
+    return (
+        <div className="container">
+            <h1>Informazioni</h1>
+        </div>
+    )
+}
--- reactrouting/src/App.js
+++ reactrouting/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Home } from './Home/Home.js';
+import { About } from './About/About.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} ></Route>
+        <Route path='/about' element={<About />} ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
